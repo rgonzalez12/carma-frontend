4618e7ebcff8fe9b18611c20994182825ce91625
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-08-04 13:59:03 -0600

    added fetch call for deleting car listings

--- src/actions/Car.js
+++ src/actions/Car.js
@@ -1,6 +1,6 @@
 export const fetchCarListings = () => {
     return dispatch => {
-      return fetch(`http://localhost:3001/cars`)
+      return fetch(`http://localhost:3000/cars`)
         .then(res => res.json())
         .then(cars => {
           dispatch({type: "GET_LISTINGS", payload: cars})
@@ -10,7 +10,7 @@ export const fetchCarListings = () => {
 
   export const addCar = car => {
     return dispatch => {
-      return fetch(`http://localhost:3001/cars`, {
+      return fetch(`http://localhost:3000/cars`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -21,6 +21,21 @@ export const fetchCarListings = () => {
         .then(car => {
   
           dispatch({type: "ADD_LISTING", payload: car})
+        })
+    }
+  }
+
+  export const deleteListing = carId => {
+    return dispatch => {
+      return fetch(`http://localhost:3000/cars/${carId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => res.json())
+        .then(carId => {
+          dispatch({type: "REMOVE_LISTING", payload: carId})
         })
     }
   }
