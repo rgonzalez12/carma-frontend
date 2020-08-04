187ad3b50295c1fe701c9de5d2aa6fbf25ac4730
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-08-04 13:56:21 -0600

    added fetch call for adding car listings

--- src/actions/Car.js
+++ src/actions/Car.js
@@ -8,4 +8,19 @@ export const fetchCarListings = () => {
     }
   }
 
+  export const addCar = car => {
+    return dispatch => {
+      return fetch(`http://localhost:3001/cars`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({car: car})
+      })
+        .then(res => res.json())
+        .then(car => {
   
+          dispatch({type: "ADD_LISTING", payload: car})
+        })
+    }
+  }
