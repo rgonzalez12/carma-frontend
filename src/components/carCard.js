a8cc0653aaf59a700a84c8de31aa3249bf348bae
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-08-06 23:12:04 -0600

    added carcard component

---
+++ src/components/carCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { deleteListing } from 'src/actions/Car.js'
+import DeleteCarButton from './DeleteButton'
+
+const CarCard = ({car, deleteListing}) => {
+  return (
+    <div className="car-card">
+      <span id="car-make">{car.make}</span> 
+      <span id="car-model">{car.model}</span> <br />
+      <span id="car-year">{car.year}</span> <br />
+      <span id="car-condition">{car.condition}</span> <br />
+      <span id="car-trim">{car.trim}</span> <br />
+      <span id="car-specs">{car.specs_and_feature}</span> <br />
+      <span id="car-image">{car.image}</span> <br />
+      <span id="car-summary">{car.summary}</span> <br />
+      <span id="car-price">{car.price}</span> <br />
+
+
+      < DeleteCarButton name="Delete Listing" car_id={car.id} deleteListing={deleteListing}/>
+      <br /><br />
+    </div>
+  )
+}
+
+export default connect(null, { deleteListing })(CarCard)
