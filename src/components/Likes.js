790d44baaa4dd9a9824b657cf3c3b999b0f3f608
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-08-28 15:00:50 -0600

    added refactoring for favorite/likes component

---
+++ src/components/Likes.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Likes extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        } 
+    }
+    handleOnClick = () => {
+        this.setState ( (state) => {
+            return { count: state.count +1 }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleOnClick}>{this.state.count}Like this Listing</button>
+            </div>
+        )
+    }
+}
+
+export default Likes
--- src/containers/Cars.js
+++ src/containers/Cars.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import CarCard from '../components/CarCard'
-
 import { connect } from 'react-redux'
 import { fetchCarListings, addCar } from '../actions'
 
